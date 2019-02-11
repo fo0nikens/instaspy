42da96644c44aa790b59bf735d2a603b73163737
H. E. Upchurch <28713121+heupchurch@users.noreply.github.com>
2019-02-11 15:51:18 -0500

    Update instaspy.py

--- instaspy.py
+++ instaspy.py
@@ -9,15 +9,15 @@ L = Instaloader()
 try:
     TARGET = argv[1]
 except IndexError:
-    raise SystemExit("Profile name required as argument.")
+    raise SystemExit("Pass profile name as argument.")
 
-##Check to see if directory exists for TARGET
+# Check to see if directory exists for TARGET
 if Path(TARGET).is_dir() == False:
     mkdir(TARGET)
 
 chdir(TARGET)
 
-##Check to see if history file exists for TARGET
+# Check to see if history file exists for TARGET
 if Path(f"{TARGET}deletionhistory.txt").is_file() == False:
     file = open(f"{TARGET}deletionhistory.txt", "w")
 
@@ -29,10 +29,16 @@ offline_posts = set(filter(lambda s: isinstance(s, Post),
 post_iterator = Profile.from_username(L.context, TARGET).get_posts()
 online_posts = set(post_iterator)
 
+#List new posts if any
 if online_posts - offline_posts:
-    print("Not yet downloaded posts:")
+    print("New posts:")
     print(" ".join(str(p) for p in (online_posts - offline_posts)))
 
+# Write list of deleted posts if any to history file
 if offline_posts - online_posts:
     file.write("Deleted posts:")
     file.write(" ".join(str(p) for p in (offline_posts - online_posts)))
+    
+# Download new posts:
+for post in Profile.from_username(L.context, TARGET).get_posts():
+    L.download_post(post, TARGET)
